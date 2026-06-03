fb64352ff6755e70c1e2613d697ec55aa5abee25
Suraj S P <161311066+DSATM1@users.noreply.github.com>
2026-06-03 09:40:34 +0000

    Comment out greet function and using only print

--- functions.py
+++ functions.py
@@ -3,8 +3,20 @@
 # print("Hello Putta! Welcome.")
 # print("Hello Ravi! Welcome.")
 
-def greet(name): #type:ignore
+"""def greet(name): #type:ignore
     print(f"Hello {name}, Good Eve!")
 
 greet("Suraj")
-greet("Putta")
+greet("Putta")"""
+
+"""def add(a,b): #type:ignore
+    print(a+b) #type:ignore
+
+result = add(5,3)
+print(result)"""
+
+def add_return(x,y): #type:ignore
+    return x+y  #type:ignore
+result = add_return(10,20)
+print(result)
+print(result*2)
