a35399d117c4a33fe678b66755dae248fb40ecf9
Suraj S P <161311066+DSATM1@users.noreply.github.com>
2026-06-03 09:32:19 +0000

    Uncomment welcome messages and define greet function in functions.py

--- functions.py
+++ functions.py
@@ -1,1 +1,10 @@
 #print("Welcome to functions")
+# print("Hello Suraj! Welcome.")
+# print("Hello Putta! Welcome.")
+# print("Hello Ravi! Welcome.")
+
+def greet(name): #type:ignore
+    print(f"Hello {name}, Good Eve!")
+
+greet("Suraj")
+greet("Putta")
